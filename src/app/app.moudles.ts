de70707cad458ffb9a29d25e36fe00494c9eb8a6
savanthps <savanth@pottsiot.com>
2025-02-01 11:33:44 +0530

    small chnages

--- src/app/app.moudles.ts
+++ src/app/app.moudles.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { FormsModule } from "@angular/forms";
 import { sharedModule } from "./shared/shared.module";
 import { TasksModule } from "./tasks/tasks.module";
 
